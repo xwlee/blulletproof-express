e321b6629cee9285393fbb2ed1a1cd0890d6f064
Lee Xiang Wei <688247+xwlee@users.noreply.github.com>
2023-08-08 18:18:29 +0800

    fix: type error

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,13 +1,16 @@
 import { User } from '@prisma/client';
 
-interface Locals {
-  err?: Error;
-}
-
 declare module 'express-serve-static-core' {
   interface Request {
     user?: Omit<User, 'password'>;
   }
+}
+
+interface Locals {
+  err?: Error;
+}
+
+declare module 'express' {
   interface Response {
     locals: Locals;
   }
